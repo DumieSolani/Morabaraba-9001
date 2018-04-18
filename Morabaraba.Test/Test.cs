a314b4a86f75cf9d1891a982eb35203cc9e2ed04
Ctnatsai <ctnatsai@gmail.com>
2018-04-18 12:25:56 +0200

    all sorted errors and other tests

--- Morabaraba.Test/Test.cs
+++ Morabaraba.Test/Test.cs
@@ -47,13 +47,6 @@ namespace Morabaraba.Test
             Assert.That((b.getBoard()).Length ==24);
         }
 
-        [Test]
-
-        public void BoardHasTwenty_FourSpaces()
-        {
-            Board b = new Board();
-            Assert.That((b.getBoard()).Length ==24);
-        }
 
 
         [Test]
@@ -124,9 +117,9 @@ namespace Morabaraba.Test
         {
             IPlayer playerX = new GamePlayer(Player.X);
             IPlayer playerO = new GamePlayer(Player.O);
-            
 
-            
+
+
             playerX.reduceNumCows();
             playerO.reduceNumCows();
 
@@ -134,63 +127,13 @@ namespace Morabaraba.Test
 
         }
 
-       
-        [Test]
-        public void PlayersStartwithTwelveCows()
-        {
-            IPlayer player1 = new GamePlayer(Player.X);
-            IPlayer player2 = new GamePlayer(Player.O);
-
-            IPlayer currPlayer1 = player1;
-            IPlayer currPlayer2 = player2;
-            Assert.That(currPlayer1.Cows == 12 && currPlayer2.Cows ==12);
-                        
-        }
-
-        [Test]
-
-        public void CheckValidPositions()
-        {
-
-            string[] validPositions = new string[] { "A1", "A4", "A7", "B2", "B4", "B6", "C3", "C4", "C5", "D1", "D2", "D3", "D5", "D6", "D7", "E3", "E4", "E5", "F2", "F4", "F6", "G1", "G4", "G7" };
-            int count = 0;
-            foreach (string item in validPositions)
-            {
-                if (Board.validPositions.Contains(item)) count++;
-            }
-
-            Assert.That(count == 24);
-
-        }
-
-        [Test]
-
-        public void CowsAreReduced()
-        {
-            IPlayer playerX = new GamePlayer(Player.X);
-            IPlayer playerO = new GamePlayer(Player.O);
-            
-
-            
-            playerX.reduceNumCows();
-            playerO.reduceNumCows();
-
-            Assert.That(playerX.Cows == 11 && playerX.Cows == 11);
-
-        }
-
-
-
-
-
-    }   
-    
+        //These tests fall under the GENERAL PHASE
         //These tests fall under the GENERAL PHASE
         [Test]
         public void AMillIsFormedByThreeCowsOfTheSameColorInALine()//Now working
         {
-        IReferee referee = new Referee();
-            
+            IReferee referee = new Referee();
+
             Board MockBoard = new Board();
 
             MockBoard.board["A1"] = new Node(Player.X);
@@ -201,29 +144,49 @@ namespace Morabaraba.Test
             //This shows that a mill can be formed by three cows of the same color in a line.
             Assert.That(MockBoard.allInMill(Player.X) == true);
 
+
+
+        }
+
+        [Test]
+        public void AMillIsNotFormedWhenThreeCowsInALineAreOfDifferentColors()//Now working
+        {
+            Referee referee = new Referee();
+
+            Board MockBoard = new Board();
+
             MockBoard.board["A1"] = new Node(Player.X);
 
             MockBoard.board["A4"] = new Node(Player.O);
 
             MockBoard.board["A7"] = new Node(Player.X);
-            //This shows that a mill cannot be formed by three cows of different colors in a line.
+
+            //This asserts that the player is not in the movement phase 
+            //as the state in the is only changed once in the movement phase.
             Assert.That(MockBoard.allInMill(Player.X) == false);
 
         }
 
         [Test]
-        public void AMillIsNotFormedWhenThreeCowsInALineAreOfDifferentColors ()//Now working
+        public void connectedSpacesContainingCowsDoNotFormALine()
         {
             Referee referee = new Referee();
 
+            Board MockBoard = new Board();
+
+            MockBoard.board["A1"] = new Node(Player.X);
+
+            MockBoard.board["B2"] = new Node(Player.O);
+
+            MockBoard.board["A7"] = new Node(Player.X);
+
             //This asserts that the player is not in the movement phase 
             //as the state in the is only changed once in the movement phase.
-            Assert.That(referee.isInMovingPhase == false);
-
+            Assert.That(MockBoard.allInMill(Player.X) == false);
         }
 
 
-
-
     }
+
 }
+
